abbe477853a09a72dbc696abf5abbd01cdce1e52
Elizabeth Setton <lizasali123@gmail.com>
2019-05-24 11:06:29 +0300

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,55 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="layoutGrid">
+    <div class="coverImg"></div>
+    <app-header></app-header>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+  import header from './components/HeaderCard/header.vue'
+  export default {
+    name: 'app',
+    components: {
+      'app-header': header
+    }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+*, *::after, *::before {
+  margin: 0 !important;
+  padding: 0!important;
+  box-sizing: inherit !important;
 }
+html {
+  font-size: 76.5% !important; //10px
+}
+
+body {
+  box-sizing: border-box !important;
+  font-family: "Lato", sans-serif;
+  font-weight: 400;
+  font-size: 1.6rem !important;
+  line-height: 1.7 !important;
+
+}
+.layoutGrid {
+  // display: grid;
+  // grid-template-rows: 80vh min-content 40vw repeat(3,min-content);
+  // grid-template-columns: [sidebar-start ]8rem [sidebar-end full-start] minmax(6rem, 1fr) [center-start] repeat(8, [col-start] minmax(min-content, 14rem) [col-end]) [center-end] minmax(6rem, 1fr) [full-end];
+  display: grid;
+  grid-template-rows: 10vh 50vh 10vh;
+  grid-template-columns: 15vw 35vw 35vw 15vw;
+}
+
+.coverImg {
+    background-image: url(./assets/cover2.jpeg);
+    background-size: cover;
+    background-position: 0 90%;
+    background-repeat: no-repeat;
+    position: relative;
+    grid-row: 1 / 3;
+    grid-column: 1 / 5;
+}
+
 </style>
---
+++ src/components/HeaderCard/header.vue
@@ -0,0 +1,71 @@
+<template>
+    <div class="card">
+        <div class="card__content row feturette">
+            <div class="col-md-5">
+                <div class="card__img">
+                    <img class="img" src="../../assets/me.jpeg" alt="profile picture">
+                </div>
+            </div>
+            <div class="col-md-7">
+                <div class="card__info">
+                    <h1>Elizabeth Setton</h1>
+                    <h4>Frontend developer</h4>
+                    <div>
+                        <dl class="dlList">
+                            <dt class="dlList__iteam">Age</dt>
+                            <dd class="dlList__iteam">22</dd>
+                            <dt class="dlList__iteam">Phone</dt>
+                            <dd class="dlList__iteam">+972 (58) 7471315</dd>
+                            <dt class="dlList__iteam">Email</dt>
+                            <dd class="dlList__iteam">Lizasali123@gmail.com</dd>
+                            <dt class="dlList__iteam">Address</dt>
+                            <dd class="dlList__iteam">Tel-Aviv, Israel</dd>
+                        </dl>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>    
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="scss">
+.card {
+    grid-row: 2 / 4;
+    grid-column: 2 / 4;
+
+    &__img {
+        padding: 3rem !important;
+        width: 100%;
+        height: 100%;
+    }
+    &__content {
+        
+    }
+}
+
+.img {
+    width: 100%;
+    height: 100%;
+}
+.dlList {
+    width: 80%;
+    overflow: hidden;
+    padding: 0;
+    margin: 0;
+
+    &__iteam {
+        float: left;
+        width: 40%;
+    }
+}
+
+h1, h4, dt {
+    text-transform: uppercase;
+}
+</style>
+
+
